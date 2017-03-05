844f9ec5df6c1425f9b43c60d85c3a7f3f70e8c2
valmynd <valmynd@gmail.com>
2017-03-05 12:53:03 +0100

    group handling works decently so far

--- tests/mathconv.js
+++ tests/mathconv.js
@@ -4,32 +4,7 @@ import {Trie} from "../dist/mathconv/trie"
 
 const parser = new Ascii2Utf8Parser()
 
-test.skip('Trie inf+i works correctly', t => {
-  const trie = new Trie()
-  trie.insert(String.raw`no\|yes|yey`, 0)
-  //console.log(JSON.stringify(trie.trie))
-  t.true(trie.trie["n"]["o"]["|"]["y"]["e"]["s"]["_v"] === 0)
-  t.true(trie.trie["y"]["e"]["y"]["_v"] === 0)
-})
-
-
-test.skip('Trie inf+i works correctly', t => {
-  const trie = new Trie()
-  trie.insert("inf+i", 1)
-  t.true(trie.trie["i"]["n"]["f"]["f"]["i"]["_v"] === 1)
-})
-
-
-test.skip('Trie infi?n?y works correctly', t => {
-  const trie = new Trie()
-  trie.insert("infi?n?y", 1)
-  //console.log(JSON.stringify(trie.trie))
-  //console.log(trie.trie)
-  t.true(trie.trie["i"]["n"]["f"]["y"]["_v"] === 1)
-  t.true(trie.trie["i"]["n"]["f"]["n"]["y"]["_v"] === 1)
-})
-
-test.skip('Trie epsil?o?n? works correctly', t => {
+test('Trie epsil?o?n? works correctly', t => {
   const trie = new Trie()
   trie.insert("epsil?o?n?", 1)
   t.true(trie.trie["e"]["p"]["s"]["i"]["_v"] === 1)
@@ -43,13 +18,26 @@ test.skip('Trie epsil?o?n? works correctly', t => {
   //console.log(JSON.stringify(trie.trie))
 })
 
-test('Trie /hel(lo)?/.exec("helo") works correctly', t => { // hel(la|(lo)+)?
+test('Trie inf+i?n?i?t?y works correctly', t => {
   const trie = new Trie()
-  //trie.insert(String.raw`hel(lo)`, 1) // the "o" node should hold the value
-  trie.insert(String.raw`hel(lo)ya`, 1) // the "a" node should hold the value
-  console.log(JSON.stringify(trie))
-  console.log(trie.trie["h"]["e"])
+  trie.insert("inf+i?n?i?t?y", 1)
+  t.deepEqual(trie.match("infinity"), {value: 1, match: "infinity"})
+  t.deepEqual(trie.match("infffity"), {value: 1, match: "infffity"})
 })
+
+test('Trie hel(lo) works correctly', t => { // hel(la|(lo)+)?
+  const trie = new Trie()
+  trie.insert(String.raw`hel(lo)`, 1)
+  t.deepEqual(trie.match("hello"), {value: 1, match: "hello"})
+})
+
+test('Trie hel(lo)?ya works correctly', t => { // hel(la|(lo)+)?
+  const trie = new Trie()
+  trie.insert(String.raw`hel(lo)?ya`, 1)
+  t.deepEqual(trie.match("helya"), {value: 1, match: "helya"})
+  t.deepEqual(trie.match("helloya"), {value: 1, match: "helloya"})
+})
+
 
 test.skip('Ascii2Utf8Parser works correctly', t => {
   t.deepEqual(parser.parse("alpha + beta * kappa"), ['α', '+', 'β', '⋅', 'κ'])
